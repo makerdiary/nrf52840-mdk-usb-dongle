381ec4117f0ce5c9d36adb5743fcc96860af6fab
Zelin Cai <zelin@makerdiary.com>
2019-03-05 17:50:57 +0800

    Add OpenThread CLI example

---
+++ examples/openthread/cli/main.c
@@ -0,0 +1,178 @@
+/**
+ * Copyright (c) 2017 - 2018, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+ *
+ * @defgroup cli_example_main main.c
+ * @{
+ * @ingroup cli_example
+ * @brief An example presenting OpenThread CLI.
+ *
+ */
+
+#include "app_scheduler.h"
+#include "app_timer.h"
+#include "bsp_thread.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log.h"
+#include "nrf_log_default_backends.h"
+
+#include "thread_utils.h"
+
+#include <openthread/thread.h>
+
+#define SCHED_QUEUE_SIZE      32                              /**< Maximum number of events in the scheduler queue. */
+#define SCHED_EVENT_DATA_SIZE APP_TIMER_SCHED_EVENT_DATA_SIZE /**< Maximum app_scheduler event size. */
+
+/***************************************************************************************************
+ * @section State
+ **************************************************************************************************/
+
+static void thread_state_changed_callback(uint32_t flags, void * p_context)
+{
+    NRF_LOG_INFO("State changed! Flags: 0x%08x Current role: %d\r\n",
+                 flags, otThreadGetDeviceRole(p_context));
+}
+
+/***************************************************************************************************
+ * @section Initialization
+ **************************************************************************************************/
+
+/**@brief Function for initializing the Application Timer Module.
+ */
+static void timer_init(void)
+{
+    uint32_t err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for initializing the LEDs.
+ */
+static void leds_init(void)
+{
+    LEDS_CONFIGURE(LEDS_MASK);
+    LEDS_OFF(LEDS_MASK);
+}
+
+
+/**@brief Function for initializing the nrf log module.
+ */
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+
+/**@brief Function for initializing the Thread Stack.
+ */
+static void thread_instance_init(void)
+{
+    thread_configuration_t  thread_configuration =
+    {
+        .role                  = RX_ON_WHEN_IDLE,
+        .autocommissioning     = false,
+        .autostart_disable     = true,
+    };
+
+    thread_init(&thread_configuration);
+    thread_cli_init();
+    thread_state_changed_callback_set(thread_state_changed_callback);
+
+    uint32_t err_code = bsp_thread_init(thread_ot_instance_get());
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for deinitializing the Thread Stack.
+ *
+ */
+static void thread_instance_finalize(void)
+{
+    bsp_thread_deinit(thread_ot_instance_get());
+    thread_soft_deinit();
+}
+
+
+/**@brief Function for initializing scheduler module.
+ */
+static void scheduler_init(void)
+{
+    APP_SCHED_INIT(SCHED_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
+}
+
+/***************************************************************************************************
+ * @section Main
+ **************************************************************************************************/
+
+int main(int argc, char *argv[])
+{
+    log_init();
+    scheduler_init();
+    timer_init();
+    leds_init();
+
+    uint32_t err_code = bsp_init(BSP_INIT_LEDS, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    while (true)
+    {
+        thread_instance_init();
+
+        while (!thread_soft_reset_was_requested())
+        {
+            thread_process();
+            app_sched_execute();
+
+            if (NRF_LOG_PROCESS() == false)
+            {
+                thread_sleep();
+            }
+        }
+
+        thread_instance_finalize();
+    }
+}
+
+/**
+ *@}
+ **/
