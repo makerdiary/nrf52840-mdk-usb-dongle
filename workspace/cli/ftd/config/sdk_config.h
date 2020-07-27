169516ff5e48de7511409ce7966b0f0712f7e5ee
Gustavo Buzogany Eboli <gbuzogany@gmail.com>
2020-07-27 14:42:03 +0200

    Disabled logging Disabled CLI Updated network configuration

--- workspace/cli/ftd/config/sdk_config.h
+++ workspace/cli/ftd/config/sdk_config.h
@@ -1369,7 +1369,7 @@
 // <e> NRF_LOG_BACKEND_RTT_ENABLED - nrf_log_backend_rtt - Log RTT backend
 //==========================================================
 #ifndef NRF_LOG_BACKEND_RTT_ENABLED
-#define NRF_LOG_BACKEND_RTT_ENABLED 1
+#define NRF_LOG_BACKEND_RTT_ENABLED 0
 #endif
 // <o> NRF_LOG_BACKEND_RTT_TEMP_BUFFER_SIZE - Size of buffer for partially processed strings. 
 // <i> Size of the buffer is a trade-off between RAM usage and processing.
@@ -1402,7 +1402,7 @@
 // <e> NRF_LOG_ENABLED - nrf_log - Logger
 //==========================================================
 #ifndef NRF_LOG_ENABLED
-#define NRF_LOG_ENABLED 1
+#define NRF_LOG_ENABLED 0
 #endif
 // <h> Log message pool - Configuration of log message pool
 
--- workspace/cli/main.c
+++ workspace/cli/main.c
@@ -76,6 +76,8 @@ static void thread_state_changed_callback(uint32_t flags, void * p_context)
 {
     NRF_LOG_INFO("State changed! Flags: 0x%08x Current role: %d\r\n",
                  flags, otThreadGetDeviceRole(p_context));
+
+    LEDS_OFF(LEDS_MASK);
 }
 
 /***************************************************************************************************
@@ -122,13 +124,12 @@ static void thread_instance_init(void)
     };
 
     thread_init(&thread_configuration);
-    thread_cli_init();
+    // thread_cli_init();
     thread_state_changed_callback_set(thread_state_changed_callback);
 
     uint32_t err_code = bsp_thread_init(thread_ot_instance_get());
     APP_ERROR_CHECK(err_code);
 }
-
 
 /**@brief Function for deinitializing the Thread Stack.
  *
@@ -139,7 +140,6 @@ static void thread_instance_finalize(void)
     thread_soft_deinit();
 }
 
-
 /**@brief Function for initializing scheduler module.
  */
 static void scheduler_init(void)
@@ -147,67 +147,34 @@ static void scheduler_init(void)
     APP_SCHED_INIT(SCHED_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
 }
 
-
 void setNetworkConfiguration(otInstance *aInstance)
 {
-    static char          aNetworkName[] = "OpenThread-a393";
     otOperationalDataset aDataset;
 
     memset(&aDataset, 0, sizeof(otOperationalDataset));
    
-    /*
-     * Fields that can be configured in otOperationDataset to override defaults:
-     *     Network Name, Mesh Local Prefix, Extended PAN ID, PAN ID, Delay Timer,
-     *     Channel, Channel Mask Page 0, Network Master Key, PSKc, Security Policy
-     */
     aDataset.mActiveTimestamp                      = 1;
     aDataset.mComponents.mIsActiveTimestampPresent = true;
      
-    /* Set Channel to 17 */
-    aDataset.mChannel                      = 17;
+    /* Set Channel to 15 */
+    aDataset.mChannel                      = 15;
     aDataset.mComponents.mIsChannelPresent = true;
     
-    /* Set Pan ID to 0xa393 */
-    aDataset.mPanId                      = (otPanId)0xA393;
+    /* Set Pan ID to 0x1234 */
+    aDataset.mPanId                      = (otPanId)0x1234;
     aDataset.mComponents.mIsPanIdPresent = true;
 
-    /* Set mMeshLocalPrefix to fda5:ecbe:8107:b60d::/64 */
-    uint8_t MeshLocalPrefix[OT_PSKC_MAX_SIZE] = {0xfd, 0xa5, 0xec, 0xbe, 0x81, 0x07, 0xb6, 0x0d};
+    /* Set mMeshLocalPrefix to fd11:1111:2222::/64 */
+    uint8_t MeshLocalPrefix[OT_PSKC_MAX_SIZE] = {0xfd, 0x11, 0x11, 0x11, 0x22, 0x22, 0x00, 0x00};
     memcpy(aDataset.mMeshLocalPrefix.m8, MeshLocalPrefix, sizeof(aDataset.mMeshLocalPrefix));
     aDataset.mComponents.mIsMeshLocalPrefixPresent = true;
 
-    /* Set mPSKc to 99 3f 68 51 16 9d 25 6b 53 62 eb df 34 41 31 d0 */
-    uint8_t PSKc[OT_PSKC_MAX_SIZE] = {0x99, 0x3F, 0x68, 0x51, 0x16, 0x9D, 0x25, 0x6B, 0x53, 0x62, 0xEB, 0xDF, 0x34, 0x41, 0x31, 0xD0};
-    memcpy(aDataset.mPskc.m8, PSKc, sizeof(aDataset.mPskc));
-    aDataset.mComponents.mIsExtendedPanIdPresent = true;
-    
-    /* Set Extended Pan ID to 0a ab 0a 1b f8 26 bb c4 */
-    uint8_t extPanId[OT_EXT_PAN_ID_SIZE] = {0x0A, 0xAB, 0x0A, 0x1B, 0xF8, 0x26, 0xBB, 0xC4};
-    memcpy(aDataset.mExtendedPanId.m8, extPanId, sizeof(aDataset.mExtendedPanId));
-    aDataset.mComponents.mIsPskcPresent = true;
-    
-    /* Set master key to 04 02 98 29 dd f0 11 b3 fd 23 50 22 9c bc fc a2 */
-    uint8_t key[OT_MASTER_KEY_SIZE] = {0x04, 0x02, 0x98, 0x29, 0xDD, 0xF0, 0x11, 0xB3, 0xFD, 0x23, 0x50, 0x22, 0x9C, 0xBC, 0xFC, 0xA2};
+    /* Set master key to 00 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF */
+    uint8_t key[OT_MASTER_KEY_SIZE] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
     memcpy(aDataset.mMasterKey.m8, key, sizeof(aDataset.mMasterKey));
     aDataset.mComponents.mIsMasterKeyPresent = true;
 
-    /* Set Network Name to OTCodelab */
-    size_t length = strlen(aNetworkName);
-    assert(length <= OT_NETWORK_NAME_MAX_SIZE);
-    memcpy(aDataset.mNetworkName.m8, aNetworkName, length);
-    aDataset.mComponents.mIsNetworkNamePresent = true;
-
-#if OPENTHREAD_FTD
     otDatasetSetActive(aInstance, &aDataset);
-    
-    /* Set the router selection jitter to override the 2 minute default.
-       CLI cmd > routerselectionjitter 20
-       Warning: For demo purposes only - not to be used in a real product */
-    uint8_t jitterValue = 20;
-    otThreadSetRouterSelectionJitter(aInstance, jitterValue);
-#else
-    OT_UNUSED_VARIABLE(aInstance);
-#endif
 }
 
 /***************************************************************************************************
@@ -244,6 +211,7 @@ int main(int argc, char *argv[])
 
             if (NRF_LOG_PROCESS() == false)
             {
+                LEDS_OFF(LEDS_MASK);
                 thread_sleep();
             }
         }
